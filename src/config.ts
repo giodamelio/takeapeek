bc92d2e32ef1598d24ea36a767cc9068512075fd
Gio d'Amelio <giodamelio@gmail.com>
2016-06-21 19:39:15 -0700

    Replace rc with nconf

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
-const rc = require('rc');
+import * as path from 'path';
+import * as os from 'os';
 
-const config = rc('takeapeek', {
-  port: 3141,
-});
+import * as nconf from 'nconf';
 
-export default config;
+// Get the config. In order of precedence
+nconf
+  .env()
+  .argv()
+
+  // A config file in the current directoy
+  .file(path.join(process.cwd(), '.takepeek.json'))
+
+  // A config file in the users home directory
+  .file(path.join(os.homedir(), '.takepeek.json'))
+
+  .defaults({
+    port: 3141,
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // This still seems a little weird see https://github.com/Microsoft/TypeScript/issues/3337
 import * as express from 'express';
+import * as nconf from 'nconf';
 
-import config from './config';
+import './config';
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(config.port, function() {
-  console.log(`takepeek listening at http://localhost:${config.port}`);
+app.listen(nconf.get('port'), function() {
+  console.log(`takepeek listening at http://localhost:${nconf.get('port')}`);
 });
