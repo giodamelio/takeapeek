6cb5f69ab2849c5cd8ee95e4e9604a0bd596318c
Gio d'Amelio <giodamelio@gmail.com>
2016-06-22 08:32:06 -0700

    Basic directory indexes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 // This still seems a little weird see https://github.com/Microsoft/TypeScript/issues/3337
 import * as path from 'path';
+import * as fs from 'fs';
 
 import * as express from 'express';
 import * as nconf from 'nconf';
+import * as handlebars from 'handlebars';
 
 import './config';
 
@@ -13,12 +15,47 @@ if (nconf.get('help')) {
   process.exit(0);
 }
 
+// Resolve the directory we are serving
+const DIRECTORY = path.resolve(process.cwd(), nconf.get('directory'));
+
+// Setup our index page
+const indexTemplate = handlebars.compile(
+  fs.readFileSync(path.resolve(__dirname, '../index.html')).toString()
+);
+
 const app = express();
 
 // Serve the directory
-app.use(express.static(
-  path.resolve(process.cwd(), nconf.get('directory'))
-));
+app.use(express.static(DIRECTORY, {
+  index: false,
+}));
+
+// Serve the index
+if (nconf.get('index')) {
+  app.use(function(req, res, next) {
+    const dir = path.join(DIRECTORY, req.url);
+    if (fs.existsSync(dir)) {
+      // List the directory
+      const children = fs.readdirSync(dir);
+
+      // Render our template
+      const page = indexTemplate({
+        directory: req.url,
+        children,
+      });
+
+      res.send(page);
+    } else {
+      next();
+    }
+  });
+}
+
+// The file or directory does not exist
+app.use(function(req, res) {
+  res.status(400);
+  res.send('The file or directory does not exist');
+});
 
 app.listen(nconf.get('port'), function() {
   console.log(`takepeek listening at http://localhost:${nconf.get('port')}`);
